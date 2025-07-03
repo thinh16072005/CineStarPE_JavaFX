d9d218fa60a059a7f6ed8ce11ddd7973cce038ab
Pham Hung Thinh <73455940+thinh16072005@users.noreply.github.com>
2025-07-03 15:07:51 +0700

    Add files via upload

---
+++ CineStarPE_SampleData.sql
@@ -0,0 +1,19 @@
+﻿-- Chèn dữ liệu vào bảng Directors
+INSERT INTO Director ( director_name, Nationality) VALUES
+( 'Phontharis', 'Thailand'),
+( 'Fujiko Fujio', 'Japan'),
+( 'James Cameron', 'USA');
+
+-- Chèn dữ liệu vào bảng Accounts
+INSERT INTO Account (Email, Password, RoleID) VALUES
+('admin@cinestar.com', '@4', 1),
+('customer@cinestar.com', '@4', 4),
+('manager@cinestar.com', '@4', 3),
+('staff@cinestar.com', '@4', 2);
+
+-- Chèn dữ liệu vào bảng Movies
+INSERT INTO Movie (movie_name, Duration, Actor, Status, DirectorID) VALUES
+('PEE NAK 4', 125, 'Witthawat', 'active', 1),
+('Doraemon', 115, 'Nobita', 'active', 2),
+('Doraemon 2022', 120, 'Doraemon', 'inactive', 2),
+('Avatar', 192, 'Kate Winslet', 'inactive', 3);
